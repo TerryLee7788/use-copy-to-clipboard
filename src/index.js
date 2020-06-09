5b842bf2cfa74612a3cd54de8ce7a0f6164b3ec8
TerryLee <k9114658@gmail.com>
2020-06-10 00:17:02 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react'
+import { useState, useRef, useCallback } from 'react'
 
 const useCopyToClipboard = () => {
     const [copyText, setCopyText] = useState('')
